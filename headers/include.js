ecdbb20035437caf45b31953ade65aa939b9d7e5
erdroc <erdroc@gmail.com>
2024-08-11 19:21:54 -0400

    test

--- headers/include.js
+++ headers/include.js
@@ -1,18 +1,14 @@
 // include.js
 
-window.addEventListener('load', function() {
-  fetch('headers/style1.html')
-    .then(response => response.text())
-    .then(data => {
-      document.head.innerHTML += data;
+fetch('headers/style1.html')
+  .then(response => response.text())
+  .then(data => {
+    document.head.innerHTML += data;
+  });
 
-      fetch('headers/header1.html')
-        .then(response => response.text())
-        .then(data => {
-          document.getElementById('header').innerHTML = data;
+fetch('headers/header1.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('header').innerHTML = data;
+  });
 
-          // Scroll to top after everything is loaded
-          window.scrollTo(0, 0);
-        });
-    });
-});
